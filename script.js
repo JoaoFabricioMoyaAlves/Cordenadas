de33841cd768d0358e584ca098429abd2ab938fe
Joao Fabrício Moya Alves <JoaoFabricioMoyaAlves@users.noreply.github.com>
2024-02-07 21:01:14 -0300

    delete de comentários inúteis

--- script.js
+++ script.js
@@ -1,49 +1,6 @@
 
 
-/*
 
-var map = L.map('map').setView([-23.5505, -46.6333], 13);
-
-var x = -23.5505 ;
-
-var y = -46.6333 ;
-
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([x, y]).addTo(map)
-    .bindPopup('Sua Localização!')
-    .openPopup();
-
-    
-
-   function pesquisa(){
-
-    
-     x = document.getElementById('latitude');
-  y = document.getElementById('longitude');
-    
-  var latitude = parseFloat(document.getElementById('latitude').value);
-  var longitude = parseFloat(document.getElementById('longitude').value);
-
-  // Atualiza as variáveis de coordenadas com os valores dos campos de formulário
-  x = latitude;
-  y = longitude;
-
-  // Remove o marcador existente do mapa
-  map.removeLayer(marker);
-
-  // Adiciona um novo marcador com as coordenadas atualizadas
-  var marker = L.marker([x, y]).addTo(map)
-      .bindPopup('Sua Localização!')
-      .openPopup();
-
-
-   }
-   
-    */
    
    var map = L.map('map').setView([-23.5505, -46.6333], 13);
 var x = -23.5505;
